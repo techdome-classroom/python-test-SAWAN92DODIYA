366dbe1323ed1e638e23c53ae23d9400c52041cf
SAWAN92DODIYA <sawandodiya02@gmail.com>
2024-01-05 13:28:13 +0530

    Problem Solution

--- program1.py
+++ program1.py
@@ -1,13 +1,16 @@
-class Solution(object):
+class Solution:
+    # @return a boolean
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
 
-
-
-  
 
--- program2.py
+++ program2.py
@@ -1,7 +1,22 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        techdome = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50, 
+            "C": 100, 
+            "D": 500,
+            "M": 1000}
+
+        res = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and  techdome[s[i]] <  techdome[s[i + 1]]:
+                res -= techdome[s[i]]
+            else:
+                res += techdome[s[i]]
+        return res
+
+
+      
